b45b8114c8b11b9ec31575a211bc956ee7635820
liam beckman <beckmanl@ohsu.edu>
2020-04-19 15:21:51 -0700

    Update date file to 'year-month-day' format.

--- withfeathers/server.py
+++ withfeathers/server.py
@@ -1,73 +1,50 @@
-# a poem presenter that draws from the light side of the force.
-# created: 2018-04-16
-# updated: 2018-04-29
-# sources:
-#   https://stackoverflow.com/questions/27587127/how-to-convert-datetime-date-today-to-utc-time
-
 from flask import Flask, request, session, g, redirect, \
         url_for, abort, render_template, flash
 from datetime import datetime
 from pathlib import Path
-#import main
+import withfeathers
 import time
 import os
-#from flask.ext.cache import Cache
-
 
 __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
-# app = Flask(__name__)
 app = Flask(__name__, static_folder='assets')
 app.config.from_object(__name__)  # load config from this file , flaskr.py
 app.config.from_envvar('WITHFEATHERS', silent=True)
-#cache = Cache(app, config={'CACHE_TYPE': 'simple'})
 
 
 # get random poem
 def update():
-    poemRandom = main.main()
+    poemRandom = withfeathers.main()
     poemCurrent = open(os.path.join(__location__, 'poemCurrent.txt'), "w");
     for line in poemRandom:
         poemCurrent.write(line)
-
-    timeCurrent = open(os.path.join(__location__, 'timeInitial.txt'), "w");
-    timeCurrent.write(str(int(time.time())))
-    print(int(time.time()))
-    # return poemCurrent
-
     poemCurrent.close()
-    timeCurrent.close()
-
 
 
 app = Flask(__name__)
 @app.route("/")
 def index():
-
     # declare initial time (YYYY-MM-DD)
-    timeInitialFile = open(os.path.join(__location__, 'timeInitial.txt'), "r");
-    timeInitialString = timeInitialFile.readline()
-    timeInitial = datetime.utcfromtimestamp(int(timeInitialString)).date()
-
+    timeFile = open(os.path.join(__location__, 'timeInitial.txt'), "r+");
     timeCurrent = datetime.utcnow().date()
+    timeInitial = datetime.strptime(timeFile.readline(), "%Y-%m-%d\n").date()
 
-    print("timeInitial:", timeInitial)
-    print("timeCurrent:", timeCurrent)
+    print(timeCurrent)
+    print(timeInitial)
 
     # if there is a new day...
     if (timeCurrent > timeInitial):
-
         # update the poem initializer
         update()
-
         # update the day
-        timeInitial = timeCurrent
+        timeFile.seek(0)
+        timeFile.write(str(timeCurrent))
 
     poemFile = open(os.path.join(__location__, 'poemCurrent.txt'), "r");
     poemString = poemFile.read()
-
     poemFile.close()
-    timeInitialFile.close()
+    timeFile.close()
     timePretty = timeCurrent.strftime("%A, %B %d, %Y")
     return render_template('index.html', content=poemString, date=timePretty)
 
@@ -81,7 +58,6 @@ def simple():
 
 
 if __name__ == "__main__":
-    #app.run()
     context=('ssl/fullchain.pem', 'ssl/privkey.pem')
-
     app.run(host="0.0.0.0", ssl_context=context, threaded=True)
+
